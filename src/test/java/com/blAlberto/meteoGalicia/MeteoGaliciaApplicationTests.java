d74c741a8103c7231fe8cf7a2395137cfd154a3d
blAlberto <albertobravolopez@gmail.com>
2019-12-27 01:15:51 +0100

    first

---
+++ src/test/java/com/blAlberto/meteoGalicia/MeteoGaliciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blAlberto.meteoGalicia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoGaliciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

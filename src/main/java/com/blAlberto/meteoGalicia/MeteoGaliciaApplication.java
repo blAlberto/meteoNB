477fe04471bd624f46a0effb210f51856279e3ae
A <A@192.168.0.14>
2020-01-07 23:13:32 +0100

    layout replace

--- src/main/java/com/blAlberto/meteoGalicia/MeteoGaliciaApplication.java
+++ src/main/java/com/blAlberto/meteoGalicia/MeteoGaliciaApplication.java
@@ -1,13 +1,19 @@
 package com.blAlberto.meteoGalicia;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import nz.net.ultraq.thymeleaf.decorators.strategies.GroupingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @SpringBootApplication
 public class MeteoGaliciaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeteoGaliciaApplication.class, args);
-	}
+    public static void main (String[] args) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addDialect(new LayoutDialect());
+        engine.addDialect(new LayoutDialect(new GroupingStrategy()));
+        SpringApplication.run(MeteoGaliciaApplication.class, args);
+    }
 
 }
